f21bec531eb2f2920f78b40be7e41361c7455536
mt320236 <mt320236@gmail.com>
2026-05-06 23:20:18 +0530

    Create SearchMatrix.java

---
+++ SearchMatrix.java
@@ -0,0 +1,14 @@
+
+// Problem: Search a 2D Matrix 
+// Approach: Start from top-right or bottom-left corner
+// Time: O(m+n) | Space: O(1)
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row=matrix.length-1;
+        int column=0;
+        while(row>=0 && column<=matrix[0].length-1){
+            if(target==matrix[row][column]){
+                return true;
+            }

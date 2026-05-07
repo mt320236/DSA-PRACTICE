4deb5d1c3e857fe333bd8c49f7eb6330114c092f
mt320236 <mt320236@gmail.com>
2026-05-07 22:01:53 +0530

    Create PalindromeNumber.java

---
+++ PalindromeNumber.java
@@ -0,0 +1,15 @@
+// Problem: Palindrome Number 
+// Approach: Convert to string, compare from both ends
+// Time: O(n) | Space: O(n)
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s=String.valueOf(x);
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i)!=s.charAt(s.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}

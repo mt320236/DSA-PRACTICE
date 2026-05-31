8e63aa1c3cc6433edc757e1a1b5035770cecd10e
mt320236 <mt320236@gmail.com>
2026-05-31 18:59:53 +0530

    Solve LeetCode 48 - Rotate Image

---
+++ RotateImage.java
@@ -0,0 +1,34 @@
+/*
+Problem:
+Rotate the given n x n matrix by 90 degrees clockwise in-place.
+
+Approach:
+1. Transpose the matrix by swapping matrix[i][j] with matrix[j][i].
+2. Reverse each row.
+3. The resulting matrix is rotated by 90 degrees clockwise.
+
+Time Complexity: O(n²)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i=0;i<matrix.length-1;i++){
+            for(int j=i+1;j<matrix.length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j]=temp;
+
+            }
+        }
+        
+    }
+}

27c3478e04049b32133bec671849f406da0937a6
Ashish Tiwari <mt320236@gmail.com>
2026-07-30 23:43:22 +0530

    Create AddTwoIntegers.java

---
+++ AddTwoIntegers.java
@@ -0,0 +1,13 @@
+/*
+Approach:
+1. Return the sum of the two given integers.
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
